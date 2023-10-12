a1f269bb543d079e1e337b0cbb6977fcbb6512f8
Macarena Villablanca <argote.andrea@gmail.com>
2023-10-12 16:22:19 -0300

    creacion router

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const express = require("express")
+const router = require("./routes/routes.js")
+const cors = require("cors")
+
 const app = express()
+
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-  res.send("¡Hola, mundo!")
-})
+app.use("/", router)
 
 app.listen(port, () => {
   console.log(`Servidor en ejecución en el puerto ${port}`)
