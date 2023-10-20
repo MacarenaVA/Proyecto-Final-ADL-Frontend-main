1ea39b8b019edb607835e029ea1922d37688a001
Macarena Villablanca <argote.andrea@gmail.com>
2023-10-19 22:55:52 -0300

    arreglo backend

--- server/app.js
+++ server/app.js
@@ -8,11 +8,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-}
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(
   "/",
