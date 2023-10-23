6cf5006c8f41290f2398729b4879ffec6a34beaa
Macarena Villablanca <argote.andrea@gmail.com>
2023-10-23 17:57:39 -0300

    limpieza app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,6 @@ import AppRouter from "./router/AppRouter"
 import Footer from "./components/Footer"
 
 function App() {
-  const [productData, setProductData] = useState([])
-  const [userData, setUserData] = useState([])
-  const [allProducts, setAllProducts] = useState([])
-  const [total, setTotal] = useState(0)
-  const [countProducts, setCountProducts] = useState(0)
-  const [user, setUser] = useState(null)
-
   return (
     <MyContextProvider>
       <Header />
