2e08660f67f816df14e80252038ebf23444ac33e
Macarena Villablanca <argote.andrea@gmail.com>
2023-10-23 18:25:45 -0300

    limpieza header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,10 +15,6 @@ const Header = () => {
   const activeClass = ({ isActive }) => (isActive ? "active" : "")
   const { isAuthenticated, logout, countProducts } = useContext(MyContext)
 
-  useEffect(() => {
-    console.log("countProducts cambió:", countProducts)
-  }, [countProducts])
-
   console.log("Header se está renderizando. countProducts:", countProducts)
 
   const handleLogout = () => {
