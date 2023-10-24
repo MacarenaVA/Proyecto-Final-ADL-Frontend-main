e784d86c31f97f3d21c3e648b70d2eea976c558a
Macarena Villablanca <argote.andrea@gmail.com>
2023-10-23 22:19:40 -0300

    actua

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div>
-      <MyContext.Provider>
+      <MyContext.Provider value={{ token }}>
         {token ? <PrivateHeader /> : <PublicHeader />}
         {token ? <PrivateRoutes /> : <PublicRoutes />}
       </MyContext.Provider>
--- client/src/Context/MyContext.jsx
+++ client/src/Context/MyContext.jsx
@@ -1,46 +1,5 @@
-import React, { createContext, useContext, useState } from "react"
+import { createContext } from "react"
 
-const MyContext = createContext()
+const MyContext = createContext({})
 
-export function useMyContext() {
-  return useContext(MyContext)
-}
-
-export function MyContextProvider({ children }) {
-  const [user, setUser] = useState(null)
-  const [cartProducts, setCartProducts] = useState([])
-  const [countProducts, setCountProducts] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [allProducts, setAllProducts] = useState([])
-  const [userPosts, setUserPosts] = useState([]) // Nueva variable
-
-  const login = (userData) => {
-    setUser(userData)
-  }
-
-  const logout = () => {
-    setUser(null)
-  }
-
-  const updateCart = (products) => {
-    setCartProducts(products)
-  }
-
-  const contextValue = {
-    user,
-    cartProducts,
-    updateCart,
-    countProducts,
-    total,
-    allProducts,
-    setAllProducts,
-    userPosts,
-    setUserPosts,
-    login,
-    logout,
-  }
-
-  return (
-    <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>
-  )
-}
+export default MyContext
