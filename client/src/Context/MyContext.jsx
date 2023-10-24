b2fc032dfef75c5bb8a4719b8036426e175a4079
Macarena Villablanca <argote.andrea@gmail.com>
2023-10-23 22:14:43 -0300

    actua

--- client/src/Context/MyContext.jsx
+++ client/src/Context/MyContext.jsx
@@ -1,5 +1,46 @@
-import { createContext } from "react"
+import React, { createContext, useContext, useState } from "react"
 
-const MyContext = createContext({})
+const MyContext = createContext()
 
-export default MyContext
+export function useMyContext() {
+  return useContext(MyContext)
+}
+
+export function MyContextProvider({ children }) {
+  const [user, setUser] = useState(null)
+  const [cartProducts, setCartProducts] = useState([])
+  const [countProducts, setCountProducts] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [allProducts, setAllProducts] = useState([])
+  const [userPosts, setUserPosts] = useState([]) // Nueva variable
+
+  const login = (userData) => {
+    setUser(userData)
+  }
+
+  const logout = () => {
+    setUser(null)
+  }
+
+  const updateCart = (products) => {
+    setCartProducts(products)
+  }
+
+  const contextValue = {
+    user,
+    cartProducts,
+    updateCart,
+    countProducts,
+    total,
+    allProducts,
+    setAllProducts,
+    userPosts,
+    setUserPosts,
+    login,
+    logout,
+  }
+
+  return (
+    <MyContext.Provider value={contextValue}>{children}</MyContext.Provider>
+  )
+}
